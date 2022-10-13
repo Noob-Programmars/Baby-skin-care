a30654e8cf042bf90f595dd36a4c1c173a2ff270
Mohankumar-10 <97840490+Mohankumar-10@users.noreply.github.com>
2022-10-13 18:12:15 +0530

    gddda

--- app/src/main/java/com/example/babyapp/charinfo.java
+++ app/src/main/java/com/example/babyapp/charinfo.java
@@ -2,15 +2,20 @@ package com.example.babyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class charinfo extends AppCompatActivity {
     Button finish;
-
+    ImageButton male,female;
+    Boolean gender; //male- true female-false
+    ImageView maleimg, femaleimg;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +28,22 @@ public class charinfo extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        male = (ImageButton) findViewById(R.id.malebutton);
+        female = (ImageButton) findViewById(R.id.femalebutton);
+        maleimg = (ImageView) findViewById(R.id.maleimg);
+        femaleimg = (ImageView) findViewById(R.id.femaleimg);
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gender = true;
+                if(gender==true){
+                    maleimg.setImageResource(R.drawable.maleon);
+                    femaleimg.setImageResource(R.drawable.femaleicon);
+                }
+            }
+        });
+
+
 
     }
 }
