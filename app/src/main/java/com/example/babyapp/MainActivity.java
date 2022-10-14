067187cb26c9c7878e283bac42acd29ae21c31f1
sakshya <sakshya.poovamma@gmail.com>
2022-10-14 14:03:37 +0530

    ihi

--- app/src/main/java/com/example/babyapp/MainActivity.java
+++ app/src/main/java/com/example/babyapp/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.myapplication.R;
+
 public class MainActivity extends AppCompatActivity {
     Button signin;
     Button register;
