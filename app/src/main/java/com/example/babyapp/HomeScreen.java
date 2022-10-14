168175bfa2209e64732edfb7f82aff4202c21840
kartheek1303 <77910960+kartheek1303@users.noreply.github.com>
2022-10-15 04:14:32 +0530

    roundedlayout

--- app/src/main/java/com/example/babyapp/HomeScreen.java
+++ app/src/main/java/com/example/babyapp/HomeScreen.java
@@ -1,9 +1,14 @@
 package com.example.babyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.app.Dialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,9 +26,8 @@ public class HomeScreen extends AppCompatActivity{
      public  int diaper_pcount = 0, hunger_pcount = 0, cribsheets_pcount = 0, cribcond_pcount = 0, balance = 100;
      public ProgressBar diaper_pBar, hunger_pbar, cribsheets_pBar, cribcond_pBar;
      public Button diaper_pbutton, hunger_pbutton, cribsheets_pbutton, cribcond_pbutton;
-     TextView txtclose;
+     TextView txtclose,warning;
      public static WeakReference<HomeScreen> weakActivity;
-     Boolean  bit7=false, bit8=false, bit9=false, bit10=false,bit11=false;
      public  ImageButton shopbutton, bathbutton, foodbutton,menubutton;
      public ImageView drawerimg,cirbimg,alertimg,toyimg;
 
@@ -37,8 +41,13 @@ public class HomeScreen extends AppCompatActivity{
         new Timer().schedule(task, 0, 1000);
         drawerimg =(ImageView) findViewById(R.id.drawerimageview);
         cirbimg = (ImageView) findViewById(R.id.cribview);
-        alertimg =(ImageView) findViewById(R.id.warningimg);
+        warning =(TextView) findViewById(R.id.warningtext);
         toyimg = (ImageView) findViewById(R.id.toyview);
+        if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("My Notification","My Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
 
         weakActivity = new WeakReference<>(HomeScreen.this);
         menubutton();
@@ -54,8 +63,7 @@ public class HomeScreen extends AppCompatActivity{
         menubutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeScreen.this,menuactivity.class);
-                startActivity(intent);
+
             }
         });
     }
@@ -127,7 +135,7 @@ public class HomeScreen extends AppCompatActivity{
             @Override
             public void onClick(View view) {
                 if (diaper_pcount < 100) {
-                    diaper_pcount = 100;
+                    diaper_pcount = 15;
                 }
 
             }
@@ -182,15 +190,24 @@ public class HomeScreen extends AppCompatActivity{
                 cribsheets_pBar.setProgress(cribsheets_pcount);
                 cribsheets_pBar.setMax(100);
             }
-            if(diaper_pcount ==0 || hunger_pcount==0 || cribcond_pcount==0 || cribsheets_pcount==0){
-                  alertimg.setImageResource(R.drawable.warningsign);
+            if(diaper_pcount==1) {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder( HomeScreen.this,"My Notification");
+                builder.setContentTitle("Your baby needs you!");
+                builder.setContentText("time to change the baby's diaper");
+                builder.setSmallIcon(R.drawable.ic_launcher_background);
+                builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(HomeScreen.this);
+                managerCompat.notify(1,builder.build());
+                warning.setText("!");
             }
-            if(diaper_pcount >0 && hunger_pcount>0 && cribcond_pcount>0 && cribsheets_pcount>0){
-                alertimg.setImageResource(0);
+            if(diaper_pcount >10 ){
+                warning.setText("");
             }
 
         }
     };
+
 
 
     public void cartbutton(){
