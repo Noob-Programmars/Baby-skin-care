f68009ec06ac9c7eb4196c2630fd8fc114e21f2f
sakshya <sakshya.poovamma@gmail.com>
2022-10-15 02:15:29 +0530

    ihi

--- app/src/main/java/com/example/babyapp/MainActivity.java
+++ app/src/main/java/com/example/babyapp/MainActivity.java
@@ -4,39 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-
-
-
 public class MainActivity extends AppCompatActivity {
-    Button signin;
-    Button register;
-    EditText email, password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        signin=(Button) findViewById(R.id.signin);
-        email=(EditText) findViewById(R.id.lpemail);
-        password=(EditText) findViewById(R.id.lppassword);
-        signin.setOnClickListener(new View.OnClickListener() {
+
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,HomeScreen.class);
-                startActivity(intent);
+            public void run() {
+                startActivity(new Intent(MainActivity.this,MainActivity2.class));
+                finish();
             }
-        });
-        register=(Button) findViewById(R.id.create);
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,registerpage.class);
-                startActivity(intent);
-            }
-        });
+        },3000);
+
 
     }
 }
