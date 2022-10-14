269649deda42335597995a903eced064ce389d9a
Mohankumar-10 <97840490+Mohankumar-10@users.noreply.github.com>
2022-10-15 02:51:01 +0530

    store

--- app/src/main/java/com/example/babyapp/menuactivity.java
+++ app/src/main/java/com/example/babyapp/menuactivity.java
@@ -2,13 +2,30 @@ package com.example.babyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class menuactivity extends AppCompatActivity {
-
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menuactivity);
+        loginwindow();
+
+    }
+    public void loginwindow(){
+        login=(Button) findViewById(R.id.button3);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(menuactivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
