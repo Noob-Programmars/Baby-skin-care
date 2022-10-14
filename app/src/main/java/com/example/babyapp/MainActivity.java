0f5c900ba3c53fff95a1ce42e83dd2e8181eb9a6
kartheek1303 <77910960+kartheek1303@users.noreply.github.com>
2022-10-14 14:41:01 +0530

    roundedlayout

--- app/src/main/java/com/example/babyapp/MainActivity.java
+++ app/src/main/java/com/example/babyapp/MainActivity.java
@@ -6,17 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-import com.example.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
     Button signin;
     Button register;
+    EditText email, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         signin=(Button) findViewById(R.id.signin);
+        email=(EditText) findViewById(R.id.lpemail);
+        password=(EditText) findViewById(R.id.lppassword);
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
