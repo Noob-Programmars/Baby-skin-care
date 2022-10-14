0a3d8704e287fb14162068b369ee1ff458e5dd3b
kartheek1303 <77910960+kartheek1303@users.noreply.github.com>
2022-10-14 20:56:18 +0530

    roundedlayout

--- app/src/main/java/com/example/babyapp/HomeScreen.java
+++ app/src/main/java/com/example/babyapp/HomeScreen.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -22,7 +24,10 @@ public class HomeScreen extends AppCompatActivity{
      public ProgressBar diaper_pBar, hunger_pbar, cribsheets_pBar, cribcond_pBar;
      public Button diaper_pbutton, hunger_pbutton, cribsheets_pbutton, cribcond_pbutton;
      TextView txtclose;
-     public  ImageButton shopbutton, bathbutton, foodbutton;
+     Boolean  bit7=false, bit8=false, bit9=false, bit10=false,bit11=false;
+     public  ImageButton shopbutton, bathbutton, foodbutton,menubutton;
+     public ImageView drawerimg,cirbimg,alertimg;
+
 
 
     @Override
@@ -31,11 +36,23 @@ public class HomeScreen extends AppCompatActivity{
         setContentView(R.layout.activity_home_screen);
         mydialog = new Dialog(this);
         new Timer().schedule(task, 0, 1000);
+        drawerimg =(ImageView) findViewById(R.id.drawerimageview);
+        alertimg =(ImageView) findViewById(R.id.warningimg);
+        menubutton();
         cartbutton();
         bathwindow();
         foodwindow();
     }
-
+    public void menubutton(){
+        menubutton = findViewById(R.id.menubutton);
+        menubutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeScreen.this,menuactivity.class);
+                startActivity(intent);
+            }
+        });
+    }
     public void Showpopup(View v) {
 
         mydialog.setContentView(R.layout.babypopup);
@@ -60,6 +77,17 @@ public class HomeScreen extends AppCompatActivity{
             }
         });
         cribprogress();
+        mydialog.show();
+    }
+    public void showpopup3(View v){
+        mydialog.setContentView(R.layout.alertlayout);
+        txtclose = (TextView) mydialog.findViewById(R.id.txtclose);
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mydialog.dismiss();
+            }
+        });
         mydialog.show();
     }
 
@@ -104,6 +132,7 @@ public class HomeScreen extends AppCompatActivity{
     TimerTask task = new TimerTask() {
         @Override
         public void run() {
+
             if(diaper_pcount>0){
                 diaper_pcount--;
                 diaper_pBar.setProgress(diaper_pcount);
@@ -123,6 +152,12 @@ public class HomeScreen extends AppCompatActivity{
                 cribsheets_pcount--;
                 cribsheets_pBar.setProgress(cribsheets_pcount);
                 cribsheets_pBar.setMax(100);
+            }
+            if(diaper_pcount ==0 || hunger_pcount==0 || cribcond_pcount==0 || cribsheets_pcount==0){
+                  alertimg.setImageResource(R.drawable.warningsign);
+            }
+            if(diaper_pcount >0 && hunger_pcount>0 && cribcond_pcount>0 && cribsheets_pcount>0){
+                alertimg.setImageResource(0);
             }
 
         }
--- app/src/main/java/com/example/babyapp/storeactivity.java
+++ app/src/main/java/com/example/babyapp/storeactivity.java
@@ -2,12 +2,129 @@ package com.example.babyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
 public class storeactivity extends AppCompatActivity {
+    Button it1, it2, it3, it4, it5, it6, it7, it8, it9, it10,it11;
+    int milkcount, foodcount, medicinecount, soapcount, shampoocount, spongecount, balance=100;
+    Boolean  bit7=false, bit8=false, bit9=false, bit10=false,bit11=false;
+    HomeScreen hs = new HomeScreen();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_storeactivity);
+        it1 = findViewById(R.id.storemilkbutton);
+        it2 = findViewById(R.id.storfoodbutton);
+        it3 = findViewById(R.id.storemedicinebutton);
+        it4 = findViewById(R.id.storeshampoobutton);
+        it5 = findViewById(R.id.storesoapbutton);
+        it6 = findViewById(R.id.storespongebutton);
+        it7 = findViewById(R.id.storetoysbutton);
+        it8 = findViewById(R.id.storedrawer2button);
+        it9 = findViewById(R.id.storedrawer3button);
+        it10 = findViewById(R.id.storecrib2button);
+        it11 = findViewById(R.id.storecrib3button);
+        button();
+
+
+    }
+    public void button(){
+        it1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(balance>=25){
+                    milkcount++;
+                    balance = balance-25;
+                }
+            }
+        });
+        it2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(balance>=25){
+                    foodcount++;
+                    balance = balance-25;
+                }
+            }
+        });
+        it3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(balance>=25){
+                    medicinecount++;
+                    balance = balance-25;
+                }
+            }
+        });
+        it4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(balance>=25){
+                    shampoocount++;
+                    balance = balance-25;
+                }
+            }
+        });
+        it5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(balance>=25){
+                    soapcount++;
+                    balance = balance-25;
+                }
+            }
+        });
+        it6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(balance>=25){
+                    spongecount++;
+                    balance = balance-25;
+                }
+            }
+        });
+        it7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (bit7 == false) {
+                    if (balance >= 50) {
+                        balance = balance - 50;
+                        bit7 = true;
+                        it7.setText("Equip");
+                    }
+                }
+            }
+
+        });
+        it8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        it9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        it10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        it11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
