8fea09ab4fb460a618ec714dd68d13349ad53da8
Mohankumar-10 <97840490+Mohankumar-10@users.noreply.github.com>
2022-10-15 05:58:54 +0530

    store

--- app/src/main/java/com/example/babyapp/menuactivity.java
+++ app/src/main/java/com/example/babyapp/menuactivity.java
@@ -10,12 +10,14 @@ import android.widget.ImageButton;
 
 public class menuactivity extends AppCompatActivity {
     Button login,quiz;
+    ImageButton home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menuactivity);
         loginwindow();
         quizwindow();
+        homewindow();
     }
     public void loginwindow(){
         login=(Button) findViewById(R.id.button3);
@@ -38,4 +40,16 @@ public class menuactivity extends AppCompatActivity {
             }
         });
     }
+    public void homewindow(){
+        home=(ImageButton) findViewById(R.id.homebutton);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(menuactivity.this,HomeScreen.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
 }
